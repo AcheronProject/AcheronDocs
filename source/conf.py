812999298d8b4afe53fe63d37115bada9e688bf0
Gondolindrim <alvaro.volpato@usp.br>
2019-04-28 03:04:57 -0300

    [Update] Ported the documentation from the SharkPCB repo

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Alvaro "Gondolindrim" Volpato'
 author = 'Alvaro "Gondolindrim" Volpato'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,7 +47,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
